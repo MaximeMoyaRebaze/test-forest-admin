4ca6d7be07fc003d7a3d57300a3102a1cfcfc6f0
Maxime <maxime#email.com>
2023-11-13 09:20:02 +0100

    standaloneserver

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
 import 'dotenv/config';
-import { createAgent } from '@forestadmin/agent';
+import { Agent, createAgent } from '@forestadmin/agent';
 import { createSqlDataSource } from '@forestadmin/datasource-sql';
-import express, { Request, Response } from 'express';
+// import express, { Request, Response } from 'express';
 
-const myExpressApp = express();
-const port = 3000;
+// const myExpressApp = express();
+// const port = 3000;
 
 // myExpressApp.get('/', (req: Request, res: Response) => {
 //     res.send('Hello, World!');
 // });
 
-myExpressApp.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// myExpressApp.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
 
 // Create your Forest Admin agent
 // This must be called BEFORE all other middleware on the myExpressApp
-createAgent({
+const agent = createAgent({
     envSecret: process.env.FOREST_ENV_SECRET ?? "",
     authSecret: process.env.FOREST_AUTH_SECRET ?? "",
     isProduction: process.env.NODE_ENV === 'production',
     typingsPath: './typings.ts',
     typingsMaxDepth: 5,
 })
-    .addDataSource(createSqlDataSource(process.env.DATABASE_URL ?? ""))
-    // .addDataSource(createSqlDataSource("postgresql://postgres:password@localhost:5432/template1"))
-    .mountOnExpress(myExpressApp)
-    // .mountOnStandaloneServer(3000)
-    .start();
+agent.addDataSource(createSqlDataSource(process.env.DATABASE_URL ?? ""))
+// agent.addDataSource(createSqlDataSource("postgresql://postgres:password@localhost:5432/template1"))
+// agent.mountOnExpress(myExpressApp)
+agent.mountOnStandaloneServer(parseInt(process.env.PORT ?? ""))
+agent.start();
