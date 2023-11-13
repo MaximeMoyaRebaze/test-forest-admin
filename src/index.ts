24bcf5bf6d5e62cf43a74089715852417ba42b62
Maxime <maxime#email.com>
2023-11-13 09:20:31 +0100

    remove express

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
 import 'dotenv/config';
-import { Agent, createAgent } from '@forestadmin/agent';
+import { createAgent } from '@forestadmin/agent';
 import { createSqlDataSource } from '@forestadmin/datasource-sql';
-// import express, { Request, Response } from 'express';
-
-// const myExpressApp = express();
-// const port = 3000;
-
-// myExpressApp.get('/', (req: Request, res: Response) => {
-//     res.send('Hello, World!');
-// });
-
-// myExpressApp.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
 
 // Create your Forest Admin agent
 // This must be called BEFORE all other middleware on the myExpressApp
@@ -24,7 +12,5 @@ const agent = createAgent({
     typingsMaxDepth: 5,
 })
 agent.addDataSource(createSqlDataSource(process.env.DATABASE_URL ?? ""))
-// agent.addDataSource(createSqlDataSource("postgresql://postgres:password@localhost:5432/template1"))
-// agent.mountOnExpress(myExpressApp)
 agent.mountOnStandaloneServer(parseInt(process.env.PORT ?? ""))
 agent.start();
