2ecb1ce6d9fd53d36b89ef0a16e44abc166d4d19
Maxime <maxime#email.com>
2023-12-06 08:57:48 +0100

    minor changes

--- src/poc/usecases/CreateACustomerAsAdmin.ts
+++ src/poc/usecases/CreateACustomerAsAdmin.ts
@@ -5,10 +5,10 @@ export const createCustomerAsAdmin:
     (CustomerService: CustomerService, firstname: string, lastname: string, address: string, phone: string) => Customer | Error =
     (CustomerService: CustomerService, firstname: string, lastname: string, address: string, phone: string) => {
         if (firstname.trim() === "" || firstname.trim().length < 3) {
-            return new Error("firstname is missing or too short")
+            return new Error("firstname is missing or too short (3 letters minimum)")
         }
         if (lastname.trim() === "" || lastname.trim().length < 3) {
-            return new Error("lastname is missing or too short")
+            return new Error("lastname is missing or too short (3 letters minimum)")
         }
         if (phone.trim() === "" || phone.trim().length !== 10) {
             return new Error("phone is missing or incorrect (10 numbers required)")
